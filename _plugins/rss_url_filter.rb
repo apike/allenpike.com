4a33421398628c15199eebf9c31a58f345d9ea2f
Allen Pike <ajpike@gmail.com>
2013-07-01 23:02:55 -0700

    Hacks to make giant images suck less in RSS.

--- _plugins/rss_url_filter.rb
+++ _plugins/rss_url_filter.rb
@@ -2,7 +2,7 @@ module Jekyll
   module RSSURLFilter
     def relative_urls_to_absolute(input)
       url = "http://www.allenpike.com"
-      input.gsub('src="/', 'src="' + url + '/').gsub('href="/', 'href="' + url + '/')
+      input.gsub('src="/', 'src="' + url + '/').gsub("src='/", "src='" + url + '/').gsub('href="/', 'href="' + url + '/').gsub("&lt;img ", "&lt;img style='max-width: 100%' ")
     end
   end
 end
